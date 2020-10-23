79b6a90a186f48e6f1a11e27acecb79ad672c932
Robinson Roa <roa.robinson97@gmail.com>
2020-10-23 19:02:35 -0400

    Second commid assignment complete

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import { Navbar, NavbarBrand } from 'reactstrap'; /* Importado por nosotros */
-import Menu from './components/MenuComponent';
+import Menu from './components/MenuComponent'; /* Importado por nosotros */
 import './App.css';
+import { DISHES } from './shared/dishes';
 
 
 class App extends Component {
+
+  constructor(props){
+
+    super(props);
+
+    this.state = {
+      dishes: DISHES
+    }
+  }
 
   render(){
     return (
@@ -15,7 +25,7 @@ class App extends Component {
             <NavbarBrand  href="/">Ristorante Con Fusion</NavbarBrand>
           </div>
         </Navbar>
-        <Menu />
+        <Menu  dishes={this.state.dishes}/>
       </div>
     );
   }
---
+++ src/components/DishdetailComponent.js
@@ -0,0 +1,87 @@
+import React, { Component } from 'react';
+import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+
+class Dishdetail extends Component {
+
+    constructor(props) {
+
+        super(props);
+        console.log('Menu compoment is invoked')
+    }
+
+    componentDidMount(){
+        console.log('Did mounted is invoked')
+
+    }
+
+    renderDish(dish){
+        if (dish != null){
+           return (
+            <div className="col-12 col-md-5 m-1">
+            <Card>
+                <CardImg width="100%" src={dish.image} alt={dish.name} />
+                <CardBody>
+                    <CardTitle heading>{dish.name}</CardTitle>
+                    <CardText>{dish.description}</CardText>
+                </CardBody>
+            </Card>
+            </div>
+            
+           ); 
+        }
+        else {
+            return(
+                <div></div>
+            );
+        }
+    }
+
+    renderComments(dish){
+
+        if (dish != null){
+
+            var listComments = dish.comments.map((d) => 
+
+            <div>
+                <p key={d.id}>{d.comment}</p>
+                <p key={d.id}>-- {d.author}, {d.date}</p>
+            </div>
+
+            );
+
+            return (
+            
+            <div className="col-12 col-md-5 m-1">
+                <h4>Comments</h4>
+                <div>{listComments}</div>
+            </div>
+          
+            )
+           
+         }
+         else {
+             return(
+                 <div></div>
+             );
+         }
+     }
+
+    
+
+
+    render() {
+        return (
+            
+            <div className="row">
+                {this.renderDish(this.props.selectedDish)}
+                {this.renderComments(this.props.selectedDish)}
+            </div>
+            
+
+        );
+    }
+
+
+}
+
+export default Dishdetail;
